aed8c3ecb1f3ec866b9792241a217d25c2152012
Turik Campbell <turik@netmille.com>
2021-12-11 12:37:23 -0600

    Updated test

--- src/test/java/com/javatpoint/SpringBootCrudOperationApplicationTests.java
+++ src/test/java/com/javatpoint/SpringBootCrudOperationApplicationTests.java
@@ -2,12 +2,19 @@ package com.javatpoint;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringBootCrudOperationApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testMe()
+	{
+		Assert.isTrue(true);
 	}
 
 }
